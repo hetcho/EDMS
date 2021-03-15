f408599259c8af37f4892cb76f7748eba75e0c69
hetcho <hetcho@mail.ru>
2021-03-15 18:45:19 +0300

    Removing MongoDao, move logic to repository

--- src/main/kotlin/ru/hetcho/edms/repositories/MongoRepository.kt
+++ src/main/kotlin/ru/hetcho/edms/repositories/MongoRepository.kt
@@ -7,9 +7,9 @@ import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.gridfs.GridFsResource
 import org.springframework.data.mongodb.gridfs.GridFsTemplate
 import org.springframework.stereotype.Repository
+import ru.hetcho.edms.entities.FileInfo
 import java.io.File
 import java.net.URLConnection
-import kotlin.jvm.Throws
 
 @Repository
 class MongoRepository(
@@ -19,15 +19,15 @@ class MongoRepository(
 
     fun create(file: File) = db.store(file.inputStream(), file.name, URLConnection.guessContentTypeFromName(file.name))
 
-    @Throws(IllegalArgumentException::class)
     fun delete(id: ObjectId) = db.delete(Query(Criteria.where("_id").`is`(id)))
+    fun delete(id: String) = delete(ObjectId(id))
 
-    @Throws(IllegalArgumentException::class)
     fun deleteAllByIds(ids: Collection<ObjectId>) = db.delete(Query(Criteria.where("_id").`in`(ids)))
+    fun deleteAll(files: Iterable<FileInfo>) = deleteAllByIds(files.map { it.objectId })
 
-    @Throws(IllegalArgumentException::class)
     fun findById(id: ObjectId) = db.findOne(Query(Criteria.where("_id").`is`(id)))
+    fun findById(id: String) = findById(ObjectId(id))
 
-    @Throws(IllegalArgumentException::class)
     fun findFileById(id: ObjectId) = GridFsResource(findById(id), bucket.openDownloadStream(id))
+    fun findFileById(id: String) = findFileById(ObjectId(id))
 }
